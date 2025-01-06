6c6948458c5cd9f24d00e0e2c0c5d8b43b7c9a65
thurunuym <marasinghe.ty@gmail.com>
2025-01-06 11:11:50 +0530

    BackEnd basics

---
+++ sportsCenterLogin/src/main/java/com/example/sportsCenterLogin/SportsCenterLoginApplication.java
@@ -0,0 +1,13 @@
+package com.example.sportsCenterLogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsCenterLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsCenterLoginApplication.class, args);
+	}
+
+}
---
+++ sportsCenterLogin/src/test/java/com/example/sportsCenterLogin/SportsCenterLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sportsCenterLogin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsCenterLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
